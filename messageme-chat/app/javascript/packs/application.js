1470d07e31f1bf3d34670cf5d469ee139636c98e
Sara Moshggoo <saramoshggoo@Saras-MacBook-Pro.local>
2020-10-23 13:38:30 -0700

    add login and route for login and root

--- messageme-chat/app/javascript/packs/application.js
+++ messageme-chat/app/javascript/packs/application.js
@@ -7,8 +7,15 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-
-
+require("semantic-ui-sass")
+$(document).on('turbolinks:load', function() {
+    $('.ui.dropdown').dropdown();
+    $('.message .close').on('click', function() {
+      $(this).closest('.message').transition('fade');
+    });
+    submit_message();
+    scroll_bottom();
+  })
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
